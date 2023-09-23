0d32e9de2732e0a6897494485b354c532576b344
emiller <karabin.m@yandex.ru>
2023-09-23 15:24:07 +0600

    [main] types update

--- react-app-env.d.ts
+++ react-app-env.d.ts
@@ -1,4 +1,17 @@
 declare module '*.png'
 declare module '*.ttf'
 declare module '*.jpg'
-declare module '*.svg'
+declare module '*.svg' {
+    import React from 'react';
+
+    const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+    export default SVG;
+}
+
+declare module '*.sass' {
+    interface IClassNames {
+        [className: string]: string;
+    }
+    const classNames: IClassNames;
+    export = classNames;
+}
