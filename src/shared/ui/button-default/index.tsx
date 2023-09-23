736ec7b739ef2e57399ccc685f1f5abb0326f94e
emiller <karabin.m@yandex.ru>
2023-09-23 22:36:35 +0600

    [main] button added

---
+++ src/shared/ui/button-default/index.tsx
@@ -0,0 +1,28 @@
+import React, {FC, HTMLAttributes, PropsWithChildren} from 'react';
+import styles from './styles.module.sass';
+import clsx from 'clsx';
+
+export interface IButtonDefaultProps extends PropsWithChildren, HTMLAttributes<HTMLButtonElement> {
+  isActive?: boolean,
+  useFullWidth?: boolean,
+}
+
+export const ButtonDefault: FC<IButtonDefaultProps> = (props) => {
+  const {className, ...otherProps} = props;
+
+  return (
+    <button
+      className={clsx([
+        styles.buttonBase,
+        props.useFullWidth && styles.fullWidth,
+        props.isActive && styles.active,
+        className,
+      ])}
+      {...otherProps}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default ButtonDefault;
