6885395e0b5ea71aec639b71ba1b09b17e369075
emiller <karabin.m@yandex.ru>
2023-09-24 18:35:03 +0600

    [main] button component refactored
    Changelog:
    * Unnecessary component removed
    * Styles updated

--- src/shared/ui/button-default/index.tsx
+++ src/shared/ui/button-default/index.tsx
@@ -2,13 +2,35 @@ import React, {FC, HTMLAttributes, PropsWithChildren} from 'react';
 import styles from './styles.module.sass';
 import clsx from 'clsx';
 
+export enum EButtonDefaultTheme {
+  primary = 1,
+  promo,
+  tab
+}
+
 export interface IButtonDefaultProps extends PropsWithChildren, HTMLAttributes<HTMLButtonElement> {
   isActive?: boolean,
   useFullWidth?: boolean,
+  theme?: EButtonDefaultTheme,
+}
+
+function getThemeClassName(theme: EButtonDefaultTheme = EButtonDefaultTheme.primary) {
+  switch (theme) {
+    case EButtonDefaultTheme.primary:
+      return styles.themePrimary;
+    case EButtonDefaultTheme.promo:
+      return styles.themePromo;
+    case EButtonDefaultTheme.tab:
+      return styles.themeTab;
+  }
 }
 
 export const ButtonDefault: FC<IButtonDefaultProps> = (props) => {
-  const {className, ...otherProps} = props;
+  const {
+    className,
+    theme = EButtonDefaultTheme.primary,
+    ...otherProps
+  } = props;
 
   return (
     <button
@@ -16,6 +38,7 @@ export const ButtonDefault: FC<IButtonDefaultProps> = (props) => {
         styles.buttonBase,
         props.useFullWidth && styles.fullWidth,
         props.isActive && styles.isActive,
+        getThemeClassName(theme),
         className,
       ])}
       {...otherProps}
