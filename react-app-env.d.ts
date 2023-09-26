71fe46349b56126158e240869321b164843ccdb1
emiller <karabin.m@yandex.ru>
2023-09-27 00:06:32 +0600

    [main] svg type fixed

--- react-app-env.d.ts
+++ react-app-env.d.ts
@@ -5,7 +5,7 @@ declare module '*.svg' {
     import React from 'react';
 
     const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
-    export default SVG;
+    export const ReactComponent = SVG;
 }
 
 declare module '*.sass' {
