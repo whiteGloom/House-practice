1ae09c590c97037ce6acf02ddd3b3a71839afd35
emiller <karabin.m@yandex.ru>
2023-10-02 00:44:23 +0600

    [main] navbar widget updated
    Changelog:
    * API file added
    * Styles updated
    * Logo widget inserted

--- src/widget/navbar/ui/navbar/index.tsx
+++ src/widget/navbar/ui/navbar/index.tsx
@@ -1,21 +1,20 @@
 import React, {FC} from 'react';
 import styles from './styles.module.sass';
-import {ReactComponent as LogoIcon} from './logo-icon.svg';
 import LinkText from '../../../../shared/ui/link-text';
-import ShoppingCartButton from '../../../../features/shopping-cart-button/ui/shopping-cart-button';
+import {ShoppingCartButton} from '../../../../features/shopping-cart-button';
+import Logo from '../../../../shared/ui/logo';
 
 export const Navbar: FC = () => {
   return (
     <nav className={styles.menu}>
-      <div className={styles.brandsWrapper}>
-        <LogoIcon className={styles.logoIcon}/>
-        <span className={styles.productTitle}>Ink. House</span>
-      </div>
-      <div className={styles.linksWrapper}>
-        <LinkText href={'#'}>Репродукции</LinkText>
-        <LinkText href={'#'}>Новинки</LinkText>
-        <LinkText href={'#'}>О нас</LinkText>
-        <ShoppingCartButton itemsCount={3} />
+      <div className={styles.content}>
+        <Logo/>
+        <div className={styles.linksWrapper}>
+          <LinkText href={'#'}>Репродукции</LinkText>
+          <LinkText href={'#'}>Новинки</LinkText>
+          <LinkText href={'#'}>О нас</LinkText>
+          <ShoppingCartButton itemsCount={3} />
+        </div>
       </div>
     </nav>
   );
