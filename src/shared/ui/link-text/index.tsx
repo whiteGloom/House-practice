5776bc54dd690e378a4a013fb47e96f7911df7f0
emiller <karabin.m@yandex.ru>
2023-10-01 22:22:37 +0600

    [main] added themes for link-text component

--- src/shared/ui/link-text/index.tsx
+++ src/shared/ui/link-text/index.tsx
@@ -2,25 +2,48 @@ import React, {FC, HTMLAttributes, PropsWithChildren} from 'react';
 import styles from './styles.module.sass';
 import clsx from 'clsx';
 
+export enum ELinkTextTheme {
+  primary = 1,
+  minor,
+}
+
 export interface ILinkTextProps extends PropsWithChildren, HTMLAttributes<HTMLAnchorElement> {
   isActive?: boolean,
   href?: string,
+  theme?: ELinkTextTheme,
+}
+
+function getThemeClassName(theme: ELinkTextTheme = ELinkTextTheme.primary) {
+  switch (theme) {
+    case ELinkTextTheme.primary:
+      return styles.themePrimary;
+    case ELinkTextTheme.minor:
+      return styles.themeMinor;
+  }
 }
 
 export const LinkText: FC<ILinkTextProps> = (props) => {
-  const {className, ...otherProps} = props;
+  const {
+    theme = ELinkTextTheme.primary,
+    children,
+    className,
+    isActive,
+    href,
+    ...otherProps
+  } = props;
 
   return (
     <a
       className={clsx([
         styles.linkBase,
         props.isActive && styles.isActive,
+        getThemeClassName(theme),
         className,
       ])}
       href={props.href}
       {...otherProps}
     >
-      {props.children}
+      {children}
     </a>
   );
 };
