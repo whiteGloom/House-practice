b800aeb26449d166ff8ce891c2f2a13b6ee0adc2
emiller <karabin.m@yandex.ru>
2023-10-02 00:42:41 +0600

    [main] logo size updated, svg component updated

--- src/shared/ui/logo/index.tsx
+++ src/shared/ui/logo/index.tsx
@@ -1,14 +1,13 @@
 import React, {FC, HTMLAttributes} from 'react';
 import styles from './styles.module.sass';
-import {ReactComponent as LogoIcon} from './logo-icon.svg';
 import clsx from 'clsx';
-
+import InkHouseLogo from '../icons/ink-house-logo';
 export const Logo: FC<HTMLAttributes<HTMLDivElement>> = (props) => {
   const {className, ...otherProps} = props;
 
   return (
     <div className={clsx([styles.wrapper, className])} {...otherProps}>
-      <LogoIcon className={styles.logoIcon}/>
+      <InkHouseLogo className={styles.logoIcon}/>
       <span className={styles.productTitle}>Ink. House</span>
     </div>
   );
