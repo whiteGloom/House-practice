19b02303bce9ea4e735f517b756ec3fee41adf6f
emiller <karabin.m@yandex.ru>
2023-10-08 15:15:22 +0600

    [main] types updated

--- react-app-env.d.ts
+++ react-app-env.d.ts
@@ -1,9 +1,17 @@
-declare module '*.png'
 declare module '*.ttf'
-declare module '*.jpg'
+
+declare module '*.jpg'{
+    const path: string;
+    export default path;
+}
+
+declare module '*.png' {
+    const path: string;
+    export default path;
+}
+
 declare module '*.svg' {
     import React from 'react';
-
     const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
     export const ReactComponent = SVG;
 }
